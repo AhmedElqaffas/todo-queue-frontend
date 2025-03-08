bba578e840b691d9e3b04023a431f7920f3d3a25
Ahmed El-Qaffas <ahmed.elqaffas97@gmail.com>
2025-03-08 21:44:49 +0200

    add commen for why 'output: export' is needed in nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
+  /* Needed for hosting on azure static web app service.
+  The service expects the site to consist of static html/css/javascript,
+  so we use 'output: export' which outputs these static components.*/
+  output: "export",
 };
 
 export default nextConfig;
